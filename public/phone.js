049c1b0afb2f41aa87b7110d603531c85080a5ee
Lewis Trundle <lewis.trundle.1510@gmail.com>
2023-01-07 16:33:24 +0000

    Add help button #7

--- public/phone.js
+++ public/phone.js
@@ -39,3 +39,27 @@ function createNipple(evt) {
 
 var joystick;
 createNipple('static');
+
+
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+console.log(btn);
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
